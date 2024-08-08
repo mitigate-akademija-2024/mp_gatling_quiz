6930bb94f0004f75e46c3344c7220bde969448f7
MuchMarts <98103825+MuchMarts@users.noreply.github.com>
2024-08-08 14:02:58 +0000

    Added Question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,34 @@
 class QuestionsController < ApplicationController
+  before_action :set_quiz, only: [:new, :create]
   def index
   end
 
   def create
+    @question = @quiz.questions.new(question_params)
+
+      if @question.save
+        flash.notice  = "Question created Success!"
+        redirect_to quiz_url(@quiz)
+      else
+        render :new, status: :unprocessable_entity
+      end
   end
 
   def start
   end
 
-  def test
+  def new
+    @question = @quiz.questions.new
   end
+
+  protected
+
+  def set_quiz
+    @quiz = Quiz.find(params[:quiz_id])
+  end
+
+  def question_params
+    params.require(:question).permit(:question_text)
+  end
+
 end
