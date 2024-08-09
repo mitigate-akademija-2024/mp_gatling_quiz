3cc2f34be3488660c5062335af8925d057c975cf
MuchMarts <98103825+MuchMarts@users.noreply.github.com>
2024-08-09 10:41:46 +0000

    Dynamic form, Answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,12 +6,17 @@ class QuestionsController < ApplicationController
   def create
     @question = @quiz.questions.new(question_params)
 
+    if params[:commit] == "add_answer"
+      @question.answers.new 
+      render :new, status: :unprocessable_entity  
+    else
       if @question.save
         flash.notice  = "Question created Success!"
         redirect_to quiz_url(@quiz)
       else
         render :new, status: :unprocessable_entity
       end
+    end
   end
 
   def start
@@ -28,7 +33,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:question_text)
+    params.require(:question).permit(:question_text, answers_attributes: [:id, :answer_text])
   end
-
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,5 @@ class Question < ApplicationRecord
 
     has_many :answers, dependent: :destroy
     belongs_to :quiz
+    accepts_nested_attributes_for :answers
 end
